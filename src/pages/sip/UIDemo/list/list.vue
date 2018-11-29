0865abc1b81dbdec3448f59e04c1b70992fe72cf
cuihaozi521 <253553244@qq.com>
2018-11-29 18:28:39 +0800

    modal

--- src/pages/sip/UIDemo/list/list.vue
+++ src/pages/sip/UIDemo/list/list.vue
@@ -1,35 +1,91 @@
 <template>
   <div class="bvue-page">
-    <b-childheader :title="header.title" :subtitle="header.description" :showBack="header.showBack"></b-childheader>
+    <b-childheader
+      :title="header.title"
+      :subtitle="header.description"
+      :showBack="header.showBack"
+    ></b-childheader>
 
-    <Icon type="ios-refresh" size="32" />
-
-    <Button type="primary" @click="createNewPage">
-      <Icon type="md-create" /> 创建
-    </Button>
-
-    <Dropdown trigger="click" @on-click="changeMenu">
-      <Button>
-        更多操作
-        <Icon type="ios-arrow-down"></Icon>
+    <Row>
+      <Col span="24">
+      <Button ><Icon  type="ios-refresh"
+        size="24" /></Button>
+     
+      <Button
+        type="primary"
+        @click="createNewPage"
+      >
+        <Icon type="md-create" /> 创建
       </Button>
-      <DropdownMenu slot="list">
-        <DropdownItem name="startup">开机</DropdownItem>
-        <DropdownItem name="shutdown">关机</DropdownItem>
-        <DropdownItem :disabled="aaa">资源移交</DropdownItem>
-        <DropdownItem>销毁保护</DropdownItem>
-        <DropdownItem divided>资源标签</DropdownItem>
-        <DropdownItem divided>制作镜像</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+       <Button type="info" ghost><Icon type="ios-play-outline" /> 开机 </Button>
+      <Button disabled><Icon type="md-power" /> 关机 </Button>
+      <Dropdown
+        trigger="click"
+        @on-click="changeMenu"
+      >
+        <Button>
+          更多操作
+          <Icon type="ios-arrow-down"></Icon>
+        </Button>
+        <DropdownMenu slot="list">
+          <DropdownItem
+            name="startup"
+            :disabled="startupDisabled"
+          >开机</DropdownItem>
+          <DropdownItem name="shutdown">关机</DropdownItem>
+          <DropdownItem :disabled="aaa">资源移交</DropdownItem>
+          <DropdownItem>销毁保护</DropdownItem>
+          <DropdownItem divided>资源标签</DropdownItem>
+          <DropdownItem divided>制作镜像</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
 
-    <Button @click="handleSelectAll(true)">全选</Button>
-    <Button @click="handleSelectAll(false)">取消全选</Button>
-    <Button @click="exportData(1)">
-      <Icon type="ios-download-outline"></Icon> 导出
-    </Button>
+      <Button @click="handleSelectAll(true)">全选</Button>
+      <Button @click="handleSelectAll(false)">取消全选</Button>
+      <Button @click="exportData(1)">
+        <Icon type="ios-download-outline"></Icon> 导出
+      </Button>
 
-    <Checkbox-group v-model="tableColumnsChecked" @on-change="changeTableColumns">
+      <Dropdown @on-click="changeTagsMenu">
+        <Button>
+          <Icon type="ios-pricetags-outline" />
+          标签
+          <Icon type="ios-arrow-down"></Icon>
+        </Button>
+        <DropdownMenu
+          v-for="item in tags"
+          slot="list"
+        >
+          <Dropdown placement="right-start">
+            <DropdownItem :name="item.key"> {{ item.name }} <Icon
+                v-if="item.children"
+                type="ios-arrow-forward"
+              ></Icon>
+            </DropdownItem>
+            <DropdownMenu slot="list">
+              <DropdownItem
+                v-for="eItem in item.children"
+                :name="eItem.key"
+              >{{ eItem.name }}</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </DropdownMenu>
+      </Dropdown>
+
+      <Input
+        style="display: inline-block;width:250px"
+        @on-search="searchEvent"
+        search
+        placeholder="搜索"
+      />
+      </Col>
+    </Row>
+
+    <Checkbox-group
+      v-model="tableColumnsChecked"
+      @on-change="changeTableColumns"
+    >
+
       <Checkbox label="name">Name</Checkbox>
       <Checkbox label="show">Show</Checkbox>
       <Checkbox label="day30">30, retained</Checkbox>
@@ -39,21 +95,69 @@
       <Checkbox label="month">Month Active</Checkbox>
     </Checkbox-group>
 
-    <Table @on-selection-change="getSelectData" :loading="loading" ref="selection" stripe highlight-row border :columns="tableColumns2" :data="tableData2"></Table>
+    <div ref="tagBlock">
+      <Tag
+        type="border"
+        v-for="(item, index) in tagsList"
+        closable
+        @on-close="handleClose(index)"
+      >{{ item }}</Tag>
+    </div>
 
-    <Modal :title="Title" v-model="modal8" :mask-closable="false" :closable="false">
-      <Alert type="warning" show-icon>
+    <Table
+      @on-select="clickStatus"
+      @on-selection-change="getSelectData"
+      :loading="loading"
+      ref="selection"
+      stripe
+      highlight-row
+      border
+      :columns="tableColumns2"
+      :data="tableData2"
+    ></Table>
+   <div style="margin: 10px;overflow: hidden">
+        <div style="float: right;">
+            <Page :total="100" :current="1" @on-change="changePage"></Page>
+        </div>
+    </div>
+    <Modal
+      :title="Title"
+      v-model="modal8"
+      :mask-closable="false"
+      :closable="false"
+    >
+      <Alert
+        type="warning"
+        show-icon
+      >
         确定要对实例【【勿删】香港区-计费-验证】开机吗?
       </Alert>
     </Modal>
 
-    <Modal v-model="modal9" :mask-closable="false" :loading="loading2" :closable="false" @on-ok="modalSave">
-      <Alert type="warning" show-icon>
+    <Modal
+      v-model="modal9"
+      :mask-closable="false"
+      :loading="loading2"
+      :closable="false"
+      @on-ok="modalSave"
+    >
+      <Alert
+        type="warning"
+        show-icon
+      >
         确定要对实例“teamview专用”进行关机操作吗?
       </Alert>
 
-      <Input v-model="desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}"></Input>
-
+      <Input
+        v-model="desc"
+        type="textarea"  style="height:500px"
+        :autosize="{minRows: 2,maxRows: 5}"
+      ></Input>
+    <div slot="footer">
+       <Button type="error" size="small"  >Delete</Button>
+            <Button type="error" size="small"  >Delete</Button>
+             <Button type="error" size="lasmallge"  >Delete</Button>
+        </div>
     </Modal>
 
   </div>
@@ -64,12 +168,26 @@
 export default {
   data: function() {
     return {
+      tags: [
+        {
+          name: "拉面",
+          key: "lamian"
+        },
+        {
+          name: "拉面2",
+          key: "lamian2",
+          children: [{ name: "a", key: "1" }, { name: "b", key: "2" }]
+        }
+      ],
+      tagsList: [],
+      searchVal: "",
       tempSelectData: null,
       desc: "",
       Title: "",
       aaa: true,
       modal8: false,
       modal9: false,
+      startupDisabled: true,
       loading2: true,
       loading: true,
       header: {
@@ -91,6 +209,18 @@ export default {
     };
   },
   methods: {
+    changeTagsMenu(name) {
+      this.tagsList.push(name);
+    },
+    handleClose(index) {
+      this.tagsList.splice(index, 1);
+    },
+    searchEvent(value) {
+      console.log(value);
+    },
+    clickStatus() {
+      this.startupDisabled = false;
+    },
     getSelectData(selection) {
       this.tempSelectData = selection;
     },
@@ -100,16 +230,16 @@ export default {
       }, 2000);
     },
     changeMenu(name) {
-      if (name == "startup") {
-        this.startup();
+      if (name == "startup" && !this.startupDisabled) {
+            this.startup();
       } else if (name == "shutdown") {
         this.shutdown();
       }
     },
     startup() {
       const selectList =
-      this.tempSelectData.length > 0 ? this.tempSelectData[0] : [];
-      const content = '<p>确定要对实例'+selectList.name+'进行开机操作吗?';
+        this.tempSelectData.length > 0 ? this.tempSelectData[0] : [];
+      const content = "<p>确定要对实例" + selectList.name + "进行开机操作吗?";
       this.$Modal.info({
         title: "开机",
         content: content
@@ -249,8 +379,12 @@ export default {
           data: this.data7.filter((data, index) => index < 4)
         });
       }
+    },
+    changePage() {
+      this.tableData2 = this.mockTableData2();
     }
   },
+  computed: {},
   mounted() {
     this.changeTableColumns();
 
